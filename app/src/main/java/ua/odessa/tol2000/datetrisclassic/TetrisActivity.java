5b973b3872e8356cfe8bb61f810d6aa0adf0475c
kay <kay@ppl33-35.net>
2017-10-10 17:24:29 +0300

    First new project DaTetrisClassic

---
+++ app/src/main/java/ua/odessa/tol2000/datetrisclassic/TetrisActivity.java
@@ -0,0 +1,13 @@
+package ua.odessa.tol2000.datetrisclassic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TetrisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tetris);
+    }
+}
